eb24d21ed357b0898ab3a2cbffd1ce7ba11ef14c
btamm12 <32679237+btamm12@users.noreply.github.com>
2017-10-17 16:19:47 +0200

    use "calculate_bpm2"
    calculate_bpm2 -> analyze_ecg2 -> find_peaks
    
    calculate_bpm -> analyze_ecg

---
+++ ECG/calculate_bpm.m
@@ -0,0 +1,19 @@
+function [ RESULT_BPM ] = calculate_bpm( INPUT_data, INPUT_fs, INPUT_maxBPM )
+%CALCULATE_BPM Summary of this function goes here
+%   Detailed explanation goes here
+
+[~, result2] = analyze_ecg(INPUT_data, INPUT_fs, INPUT_maxBPM);
+
+if length(result2) > 1
+    firstBeat = result2(1);
+    lastBeat = result2(length(result2));
+    
+    % # Beats = length(result2) - 1
+    % # Minutes = # samples 
+    RESULT_BPM =  (length(result2) - 1)/((lastBeat - firstBeat) / INPUT_fs / 60);
+else
+    RESULT_BPM = 0;
+end
+
+end
+
