c5ec48b1b8b6c9f60e87b4b3a5ddefc1a5d8fdbf
Pieter <pieter.pas@outlook.com>
2017-10-18 17:56:43 +0200

    Add noise to signal Check false positive @18145

--- ECG/testAgainstMITBIH.m
+++ ECG/testAgainstMITBIH.m
@@ -4,14 +4,22 @@ dataset = '103';
 nb_samples = 360*100; % how many samples to test
 [ samplefreq, dualecg, annotations, annotationSamples ] = readSignalAndAnnotations('../BIH/', dataset, nb_samples); % directory containing data, dataset number, number of samples
 
-ecg = dualecg(:,1); % only look at the first signal
+samples = 1:nb_samples; % X axis
+rawecg = dualecg(:,1)'; % only look at the first signal
 Rpeaks = annotationSamples(annotations(:) == 1)+1; % only use the annotations for R-peaks (https://www.physionet.org/physiotools/wfdb/lib/ecgcodes.h) TODO
 
+time = samples/samplefreq;
+whitenoise = 60*wgn(nb_samples,1,0)';
+noise = 360*sin(time*2*pi/2) + 36*sin(time*2*pi*50) + whitenoise; % 0.5 and 50 Hz + white gaussian noise
+
+ecg = rawecg;
+ecg = ecg + noise;
+
 figure
-samples = 1:nb_samples; % X axis
 hold on
-plot(samples, ecg) % plot the raw ECG signal
-scatter(Rpeaks, ecg(Rpeaks),50,'b') % plot the expert annotations
+plot(samples, ecg + 1000)
+plot(samples, rawecg) % plot the raw ECG signal
+scatter(Rpeaks, rawecg(Rpeaks),50,'b') % plot the expert annotations
 
 waveletFilteredECG = waveletFilterECG(ecg, 5, [0,0,1,1,1,0]); % perform the wavelet transformation filter
 plot(waveletFilteredECG); % plot the wavelet filtered signal
@@ -19,4 +27,4 @@ plot(waveletFilteredECG); % plot the wavelet filtered signal
 maxBPM = 200; % Max BPM expected
 [bpm, locations] = calculate_bpm2(waveletFilteredECG', samplefreq, maxBPM); % find the peaks and BPM
 
-scatter(locations, ecg(locations),20,'r') % plot the detected R-peaks
+scatter(locations, rawecg(locations),20,'r') % plot the detected R-peaks
