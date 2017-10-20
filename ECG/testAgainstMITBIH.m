1afbee7e6f800c5c5a23bd0f3b4446ed5bcf021e
Pieter <pieter.pas@outlook.com>
2017-10-20 14:04:29 +0200

    Add automatic test against MIT annotations

--- ECG/testAgainstMITBIH.m
+++ ECG/testAgainstMITBIH.m
@@ -1,11 +1,13 @@
-addpath('../BIH')
-dataset = '103';
+close all;
 
-nb_samples = 360*100; % how many samples to test
+addpath('../BIH')
+dataset = '100';
+
+nb_samples = 360*10; % how many samples to test
 [ samplefreq, dualecg, annotations, annotationSamples ] = readSignalAndAnnotations('../BIH/', dataset, nb_samples); % directory containing data, dataset number, number of samples
 
 samples = 1:nb_samples; % X axis
-rawecg = dualecg(:,1)'; % only look at the first signal
+rawecg = dualecg(:,2)'; % only look at the first signal
 Rpeaks = annotationSamples(annotations(:) == 1)+1; % only use the annotations for R-peaks (https://www.physionet.org/physiotools/wfdb/lib/ecgcodes.h) TODO
 
 time = samples/samplefreq;
@@ -13,7 +15,7 @@ whitenoise = 60*wgn(nb_samples,1,0)';
 noise = 360*sin(time*2*pi/2) + 36*sin(time*2*pi*50) + whitenoise; % 0.5 and 50 Hz + white gaussian noise
 
 ecg = rawecg;
-ecg = ecg + noise;
+% ecg = ecg + noise;
 
 figure
 hold on
@@ -22,10 +24,38 @@ plot(samples, rawecg) % plot the raw ECG signal
 scatter(Rpeaks, rawecg(Rpeaks),50,'b') % plot the expert annotations
 
 waveletFilteredECG = waveletFilterECG(ecg, 5, [0,0,1,1,1,0]); % perform the wavelet transformation filter
-plot(waveletFilteredECG); % plot the wavelet filtered signal
+plot(waveletFilteredECG.^2/200); % plot the square of the wavelet filtered signal
+plot(waveletFilteredECG-500); % plot the wavelet filtered signal
+
 
 % Max BPM expected
 maxBPM = 150;        
 [bpm, locations] = calculate_bpm3(waveletFilteredECG', samplefreq, maxBPM);
 
 scatter(locations, rawecg(locations),20,'r') % plot the detected R-peaks
+
+%%
+missed = 0;
+falsePositive = 0;
+
+expertIndex = 1;
+testIndex = 1;
+
+while (expertIndex <= length(Rpeaks) && testIndex <= length(locations))
+    if (abs(Rpeaks(expertIndex) - locations(testIndex)) > 4)
+        if (Rpeaks(expertIndex) > locations(testIndex))
+            falsePositive = falsePositive + 1;
+            disp ("False positive:");
+            disp(locations(testIndex));
+            testIndex = testIndex + 1;
+        else
+            missed = missed + 1;
+            disp ("Missed:");
+            disp(Rpeaks(expertIndex));
+            expertIndex = expertIndex + 1;
+        end 
+    else
+        testIndex = testIndex + 1;
+        expertIndex = expertIndex + 1;
+    end
+end
