fef4ca93ef2bd8e2409e31a51b28aaa21bcacfec
Pieter Pas <pieter.pas@outlook.com>
2017-10-29 00:22:07 +0200

    Read serial port working cb, too slow

--- Arduino/ReadSerialPort.m
+++ Arduino/ReadSerialPort.m
@@ -1,58 +1,67 @@
 function main
-addpath('../GUI & Tests/')
-global bufferlen ecgbuffer msgtype value t xtime
-msgtype = 0;
-value = uint16(0);
+global h value msgtype ecgbuffer bufferlen
+    addpath('../GUI & Tests/')
+    close all
 
-samplefreq = 360;
-windowsize = 10;
+    msgtype = 0;
+    value = uint16(0);
 
-bufferlen = windowsize * samplefreq;
-ecgbuffer = zeros(bufferlen,1);
+    samplefreq = 360;
+    windowsize = 10;
 
-t = testapp;
+    bufferlen = windowsize * samplefreq;
+    ecgbuffer = zeros(bufferlen,1);
 
-t.UIAxes.YLim = [0, 1023];
-xtime = (0:bufferlen-1)/samplefreq;
-xtime = windowsize - xtime;
+    %t = testapp;
 
-fclose(instrfind);
-delete(instrfind);
-clear instrfind;
-instrfind
-s = serial('COM9','BaudRate',115200);
-s.BytesAvailableFcnCount = 2;
-s.BytesAvailableFcnMode = 'byte';
-s.BytesAvailableFcn = @serialevent;
-fopen(s);
-tic;
-while (s.Status ~= 'open') && (toc < 5000)
-end
-if toc > 5000
-    disp("Error opening port")
-else 
-    disp("Port open")
-end
-pause(10);
-end
+    %t.UIAxes.YLim = [0, 1023];
+    %xtime = (0:bufferlen-1)/samplefreq;
+    %xtime = windowsize - xtime;
 
-function [] = serialevent(obj,event,time)
-global bufferlen ecgbuffer msgtype value t xtime
-    x = uint16(fread(obj, 1));
-    
-    if bitand(x, 128) % 128 == 0b10000000
-        % if it's a header byte
-        msgtype = bitand(x, 96); % 96 == 0b01100000
-        value = bitshift(bitand(x, 7), 7); % 7 == 0b111 
-    else 
-        % if it's a data byte
-        value = bitor(value, x);
-        if msgtype == 0 % ECG signal
-            ecgbuffer(1:(bufferlen-1)) = ecgbuffer(2:bufferlen);
-            ecgbuffer(bufferlen) = value;
-            value = uint16(0);
-            assignin('base', 'ecgbuffer', ecgbuffer);
-%            plot(t.UIAxes, xtime, ecgbuffer);
-        end
+    instrfind
+    if ~isempty(instrfind)
+        fclose(instrfind);
+        delete(instrfind);
+        clear instrfind;
     end
+
+    s = serial('COM9','BaudRate',1000000);
+    
+    s.BytesAvailableFcn = @cb;
+    s.BytesAvailableFcnCount = 1;
+    s.BytesAvailableFcnMode = 'byte';
+
+    fopen(s);
+
+    h = axes;
+    while ishandle(h)
+
+    end
+end
+
+function cb (s, event, time)
+global h value msgtype ecgbuffer bufferlen
+    %if s.BytesAvailable >= 1
+    %    s.BytesAvailable
+        x = uint16(fread(s, 1));
+
+        if bitand(x, 128) ~= 0 % 128 == 0b10000000
+            % if it's a header byte
+            msgtype = bitand(x, 7); % 7 == 0b0111
+            value = bitshift(bitand(x, 112), 3); % 112 == 0b01110000 
+        else 
+            % if it's a data byte
+            value = bitor(value, x);
+            if msgtype == 0 % ECG signal
+                ecgbuffer(1:(bufferlen-1)) = ecgbuffer(2:bufferlen);
+                ecgbuffer(bufferlen) = value;
+                value;
+                value = uint16(0);
+                if ishandle(h)
+                    plot(h, ecgbuffer);
+                    drawnow
+                end
+            end
+        end
+    %end
 end
