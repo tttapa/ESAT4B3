e411e570c37591eaa5151c1d6c0b1aca5ee047e5
Pieter Pas <pieter.pas@outlook.com>
2017-11-07 16:58:47 +0100

    Add comments ReadSerialPort.m

--- Arduino/ReadSerialPort.m
+++ Arduino/ReadSerialPort.m
@@ -3,39 +3,34 @@ global h value msgtype ecgbuffer bufferlen
     addpath('../GUI & Tests/')
     close all
 
-    msgtype = 0;
-    value = uint16(0);
+    msgtype = 0; % initialization of variables used in callback functions 
+    value = uint16(0); % "
 
     samplefreq = 360;
-    windowsize = 10;
+    windowsize = 10; % show 10 seconds of data
 
     bufferlen = windowsize * samplefreq;
-    ecgbuffer = zeros(bufferlen,1);
-
-    %t = testapp;
-
-    %t.UIAxes.YLim = [0, 1023];
-    %xtime = (0:bufferlen-1)/samplefreq;
-    %xtime = windowsize - xtime;
-
-    instrfind
-    if ~isempty(instrfind)
+    ecgbuffer = zeros(bufferlen,1); % create an empty buffer
+    
+    instrfind % List serial ports
+    if ~isempty(instrfind) % Close and delete open serial ports
         fclose(instrfind);
         delete(instrfind);
         clear instrfind;
     end
 
+    % Add a serial port
     if isunix % Linux
         s = serial('/dev/ttyACM0','BaudRate',1000000);
     elseif ispc % Windows
         s = serial('COM9','BaudRate',1000000);
     end
     
-    s.BytesAvailableFcn = @cb;
-    s.BytesAvailableFcnCount = 1;
+    s.BytesAvailableFcn = @cb; % Callback function
+    s.BytesAvailableFcnCount = 1; % On every byte received
     s.BytesAvailableFcnMode = 'byte';
 
-    fopen(s);
+    fopen(s); % Open the serial port
 
     h = axes;
     while ishandle(h)
@@ -43,29 +38,28 @@ global h value msgtype ecgbuffer bufferlen
     end
 end
 
-function cb (s, event, time)
+function cb (s, event, time) % Callback function that executes when a new byte arrives on the serial port
 global h value msgtype ecgbuffer bufferlen
-    %if s.BytesAvailable >= 1
-    %    s.BytesAvailable
-        x = uint16(fread(s, 1));
+    % s.BytesAvailable
+    x = uint16(fread(s, 1)); % Read a byte
 
-        if bitand(x, 128) ~= 0 % 128 == 0b10000000
-            % if it's a header byte
-            msgtype = bitand(x, 7); % 7 == 0b0111
-            value = bitshift(bitand(x, 112), 3); % 112 == 0b01110000 
-        else 
-            % if it's a data byte
-            value = bitor(value, x);
-            if msgtype == 0 % ECG signal
-                ecgbuffer(1:(bufferlen-1)) = ecgbuffer(2:bufferlen);
-                ecgbuffer(bufferlen) = value;
-                value;
-                value = uint16(0);
-                if ishandle(h)
-                    plot(h, ecgbuffer);
-                    drawnow
-                end
+    % https://github.com/tttapa/ESAT4B3/blob/master/Arduino/Serial-Protocol.md
+    if bitand(x, 128) ~= 0 % 128 == 0b10000000
+        % if it's a header byte
+        msgtype = bitand(x, 7); % 7 == 0b0111
+        value = bitshift(bitand(x, 112), 3); % 112 == 0b01110000 
+    else 
+        % if it's a data byte
+        value = bitor(value, x);
+        if msgtype == 0 % ECG signal
+            ecgbuffer(1:(bufferlen-1)) = ecgbuffer(2:bufferlen); % shift the buffer
+            ecgbuffer(bufferlen) = value; % add the new value to the buffer
+            value;
+            value = uint16(0);
+            if ishandle(h)
+                plot(h, ecgbuffer); % Plot the buffer
+                drawnow
             end
         end
-    %end
+    end
 end
