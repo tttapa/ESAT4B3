594beae3a82714da7658b2cf0e44eb18b17eacc5
btamm12 <32679237+btamm12@users.noreply.github.com>
2017-11-10 14:31:57 +0100

    analyzePPG

---
+++ PPG/analyzePPG.m
@@ -0,0 +1,38 @@
+function [ RESULT_peaks, RESULT_locations ] = analyzePPG( INPUT_data, INPUT_fs, INPUT_maxBPM )
+%
+% ANALYZE_ECG returns a list of peaks and their respective locations in the
+% data.
+%
+% @param INPUT_data - column vector of data
+% @param INPUT_fs - sample rate
+% @param INPUT_maxBPM - highest expected BPM
+%
+
+
+%%
+% SETUP
+%
+
+% Minimum expected samples per heartbeat using sample rate & max BPM
+minHeartbeatLength = floor(INPUT_fs * 60 / INPUT_maxBPM);
+
+% Percentage of highest value that will be the cut-off value
+cutOffFactor = 0.4;
+
+
+%%
+% EXECUTION
+%
+    
+% Find the highest value in the interval, and calculate the cut-off value
+highestValue = max(INPUT_data);
+cutOffValue = highestValue*cutOffFactor;
+
+% Find the peaks within this interval
+%   -> Parameter 3: minDistance must be <= length(INPUT_data) - 2
+[RESULT_peaks, RESULT_locations] = getPeaks(INPUT_data, cutOffValue, min([minHeartbeatLength, length(INPUT_data) - 2]));
+
+end
+
+
+
