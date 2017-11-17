603653233a4cf122a9de151b686002fbd9b0199a
Pieter <pieter.pas@outlook.com>
2017-11-17 14:12:41 +0100

    Add HSI to RGB

---
+++ GUI & Tests/HSI.cpp
@@ -0,0 +1,21 @@
+void setHue(int hue) { // Set the RGB LED to a given hue (color) (0° = Red, 120° = Green, 240° = Blue)
+  hue %= 360;                   // hue is an angle between 0 and 359°
+  float radH = hue*3.142/180;   // Convert degrees to radians
+  float rf, gf, bf;
+  
+  if(hue>=0 && hue<120){        // Convert from HSI color space to RGB              
+    rf = cos(radH*3/4);
+    gf = sin(radH*3/4);
+    bf = 0;
+  } else if(hue>=120 && hue<240){
+    radH -= 2.09439;
+    gf = cos(radH*3/4);
+    bf = sin(radH*3/4);
+    rf = 0;
+  } else if(hue>=240 && hue<360){
+    radH -= 4.188787;
+    bf = cos(radH*3/4);
+    rf = sin(radH*3/4);
+    gf = 0;
+  }
+}
