a99fdbc1f6cfea5d424302ed36c632605579716f
KatrienVanDaele <33153381+KatrienVanDaele@users.noreply.github.com>
2017-11-21 18:05:40 +0100

    attempt daily_steps

--- Druksensor/daily_steps.m
+++ Druksensor/daily_steps.m
@@ -1,7 +1,5 @@
 
-function[bar_steps] = daily_steps(steps)        
-x = datestr(steps(1,:), 'HH:MM');               %x: vanaf begin, om de 4 weergeven
-bar_steps = bar(x(1,4,:), steps(:,2), 0.8));
+function[bar_steps] = daily_steps(steps)                                   %aantal stappen per kwartier opgeslagen in excelfile
+x = datestr(steps(1,:), 'HH:MM');                                          %1e kolom: tijd, 2e kolom: aantal stappen
+bar_steps = bar(x, steps(:,2), 0.8);                                       %x: vanaf begin, pas om de 4 weergeven? (indexing?)
 end
-             
-%1e kolom: tijd, 2e kolom: aantal stappen
