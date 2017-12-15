3d318a57ba8c7e882037cc3e74eaa9fe10fe1ba4
Pieter Pas <pieter.pas@outlook.com>
2017-12-15 02:20:37 +0100

    add plots for real time peak detection

---
+++ Visualization/RealTimePeakDetector.m
@@ -0,0 +1,41 @@
+close all;
+data = csvread('../Data/RealDataArduino.csv');
+addpath('../Def');
+
+coeff = ECG_setup(360);
+
+data = data -250;
+
+filtered = ECG_filter(data, coeff);
+
+data = data(100:100+360*2);
+filtered = filtered(100:100+360*2);
+
+sq = ((filtered).^2) / 1023;
+difsq = filter([1 -1], 1, sq);
+dif = filter([1 -1], 1, filtered);
+
+figure, hold on,
+plot(sq,'k','LineWidth',1);
+plot([0, length(sq)],[30 30], '--k');
+title('Square of the filtered ECG signal');
+xlabel('Sample');
+ylabel('Amplitude');
+xlim([1 720]);
+
+fig = figure;
+left_color = [0 0 0];
+right_color = [1 0 0];
+set(fig,'defaultAxesColorOrder',[left_color; right_color]);
+hold on;
+yyaxis left,
+plot(dif,'k','LineWidth',1);
+ylabel('Amplitude');
+yyaxis right,
+plot(difsq,'r','LineWidth',1);
+plot([0, length(dif)],[6 6], '--k');
+title('Discrete derivative of the filtered ECG signal and the square of the filtered ECG signal');
+xlabel('Sample');
+ylabel('Amplitude');
+xlim([1 720]);
+legend('Discrete derivative of the filtered ECG signal','Discrete derivative of the square of the filtered ECG signal', 'Location','southeast');
