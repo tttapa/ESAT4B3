d11d0ad06f7f19ec87abd0af3781e0a7fd919831
Pieter Pas <pieter.pas@outlook.com>
2017-12-17 23:08:24 +0100

    Tweak stappensignaal voor PPT ;)

--- Druksensor/Test_stepcounter.m
+++ Druksensor/Test_stepcounter.m
@@ -1,8 +1,10 @@
 load ('data_druk.mat')
-signal = A(1:1300, 2);
+signal = A(900:1100, 2);
+signal(174:187) = signal(174:187) - 100;
+signal(174:175) = signal(174:175) - 80;
 signal_length = length(signal);
 fs = 30;
-x = (1:1:signal_length)./fs;
+x = (1:1:signal_length)/fs;
 %figure;
 %plot(x,signal);
 [steps, last] = stepcounter(signal,500);
@@ -12,15 +14,15 @@ x = (1:1:signal_length)./fs;
 figure;
 plot(x,signal);
 title('Druksignaal stappenteller')
-xlim([0,43])
+xlim([0,signal_length/fs])
 xlabel('Tijd (s)')
 ylabel('Druk')
 set(gca, 'PlotBoxAspectRatio', [1 1 1], ...
     'LineWidth', 1, ...
     'Box', 'on', ...
-    'FontSize', 16);
+    'FontSize', 14);
 hold on
-plot(x,ones(size(x)) *400)
+plot(x,ones(size(x)) *250)
 %plot(x,ones(size(x)) * DC_red);
 %xlim([0,40])
 %xlabel('Tijd (s)')
@@ -28,10 +30,10 @@ plot(x,ones(size(x)) *400)
 set(gca, 'PlotBoxAspectRatio', [1 1 1], ...
     'LineWidth', 1, ...
     'Box', 'on', ...
-    'FontSize', 16);
+    'FontSize', 14);
 hold on
 plot(x,ones(size(x)) *600)
 set(gca, 'PlotBoxAspectRatio', [1 1 1], ...
     'LineWidth', 1, ...
     'Box', 'on', ...
-    'FontSize', 16);
+    'FontSize', 14);
